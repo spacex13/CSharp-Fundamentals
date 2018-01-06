88a25bcf4f6f8fb9d0e67921de5c5dd7c2af5527
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-07 00:33:08 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Lab/2.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.SimpleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int first = int.Parse(stack.Pop());
+                String op = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                switch (op)
+                {
+                    case "+":
+                        stack.Push((first + second).ToString());
+                        break;
+                    case "-":
+                        stack.Push((first - second).ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
