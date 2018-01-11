a6269c75c9b22559367f6991321c7009319f55e9
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-11 18:37:00 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Lab/3.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.DecimalToBinaryConverter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            if (num == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                while (num != 0)
+                {
+                    stack.Push(num % 2);
+                    num /= 2;
+                }
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());    
+            }
+            Console.WriteLine();
+        }
+    }
+}
