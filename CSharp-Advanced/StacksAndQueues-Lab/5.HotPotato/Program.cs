d0da69f28372d0b9f86d62368d710589b73459a9
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-14 11:09:44 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Lab/5.HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.HotPotato
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(input.Split());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    string last = queue.Dequeue();
+                    queue.Enqueue(last);
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
---
+++ CSharp-Advanced/StacksAndQueues-Lab/6.MathPotato/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.MathPotato
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(input.Split());
+
+            int cycle = 1;
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                if (PrimeTool.IsPrime(cycle))
+                {
+                    Console.WriteLine($"Prime {queue.Peek()}");
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {queue.Dequeue()}");
+                }
+                cycle++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+
+        public static class PrimeTool
+        {
+            public static bool IsPrime(int candidate)
+            {
+                if ((candidate & 1) == 0)
+                {
+                    if (candidate == 2)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+               
+                for (int i = 3; (i * i) <= candidate; i += 2)
+                {
+                    if ((candidate % i) == 0)
+                    {
+                        return false;
+                    }
+                }
+                return candidate != 1;
+            }
+        }
+    }
+}
