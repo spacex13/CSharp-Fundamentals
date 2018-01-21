26a0644082485e035de55d9e543c5fb9ad670ff1
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-21 12:51:18 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/03.MaximumElement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumElement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+            int n = int.Parse(Console.ReadLine());
+            int max = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                switch (input[0])
+                {
+                    case 1:
+                        stack.Push(input[1]);
+                        if (input[1] > max)
+                        {
+                            max = input[1];
+                        }
+                        break;
+                    case 2:
+                        int element = stack.Pop();
+
+                        if (element == max && stack.Count > 0)
+                        {
+                            max = stack.Max();
+                        }
+                        else if (element == max && stack.Count == 0)
+                        {
+                            max = 0;
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine(max);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/StacksAndQueues-Exercise/04.BasicQueueOperations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            Queue<int> queue = new Queue<int>(nums);
+
+            for (int i = 0; i < s; i++)
+            {
+               queue.Dequeue(); 
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else 
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
