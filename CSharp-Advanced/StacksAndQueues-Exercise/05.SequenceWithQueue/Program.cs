cfd1a88469a8badcc493f71aec5066e04a10c202
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-22 17:23:40 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/05.SequenceWithQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SequenceWithQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+            var sequence = new Queue<long>();
+            var seqNums = new List<long> {n};
+
+            sequence.Enqueue(n);
+            while (seqNums.Count < 50)
+            {
+                var firstItem = sequence.Peek() + 1;
+                sequence.Enqueue(firstItem);
+                var secondItem = sequence.Peek() * 2 + 1;
+                sequence.Enqueue(secondItem);
+                var thirdItem = firstItem + 1;
+                sequence.Enqueue(thirdItem);
+
+                sequence.Dequeue();
+                seqNums.Add(firstItem);
+                seqNums.Add(secondItem);
+                seqNums.Add(thirdItem);
+            }
+
+            Console.WriteLine(string.Join(" ", seqNums.Take(50)));
+        }
+    }
+}
