04224d4c19f6bd60a21c6d9164e49b27757fc3ea
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-27 22:10:45 +0200

    added matrices exercises

---
+++ CSharp-Advanced/MultidimensionalArrays-Lab/1.SumMatrixElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace MultidimensionalArrays_Lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(new []{", "}, StringSplitOptions.None).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var info = Console.ReadLine().Split(new [] {", "}, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for (int col   = 0; col < cols; col++)
+                {
+                    matrix[row, col] = info[col];
+                }
+            }
+
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
