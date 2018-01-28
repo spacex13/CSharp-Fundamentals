10b5c053f1afc7ae008e21ce2ab7e033efb6fab2
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-01-28 21:39:32 +0200

    added exercises

---
+++ CSharp-Advanced/MultidimensionalArrays-Lab/3.GroupNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.GroupNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+
+            int[] length = new int[3];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                length[Math.Abs(input[i] % 3)]++;
+            }
+
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[length[0]];
+            jaggedArray[1] = new int[length[1]];
+            jaggedArray[2] = new int[length[2]];
+
+            int[] offsets = new int[3];
+
+            foreach (var number in input)
+            {
+                int remainder = number % 3;
+                remainder = Math.Abs(remainder);
+                int index = offsets[remainder];
+                jaggedArray[remainder][index] = number;
+                offsets[remainder]++;
+            }
+
+            for (int i = 0; i < length.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/MultidimensionalArrays-Lab/4.PascalTriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _4.PascalTriangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] jaggedArray = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jaggedArray[i] = new long[i + 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int len = jaggedArray[i].Length;
+                jaggedArray[i][0] = 1;
+                jaggedArray[i][len - 1] = 1;
+
+                if (i >= 2)
+                {
+                    for (int j = 1; j < len - 1; j++)
+                    {
+                        jaggedArray[i][j] = jaggedArray[i - 1][j - 1] + jaggedArray[i - 1][j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
