68bd422d6fbe8951b488e73bd830aa303763c338
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-01 16:35:26 +0200

    added exercises

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/07.BalancedParentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.BalancedParentheses
+{
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+
+            char[] leftBr = { '{', '(', '[' };
+            char[] rightBr = { '}', ')', ']' };
+
+            Stack<char> stack = new Stack<char>();
+            bool isBalanced = true;
+
+            if (stack.Count % 2 != 0)
+            {
+                isBalanced = false;
+            }
+            else
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (leftBr.Contains(line[i]))
+                    {
+                        stack.Push(line[i]);
+                    }
+                    else if (rightBr.Contains(line[i]))
+                    {
+                        if (stack.Count == 0)
+                        {
+                            isBalanced = false;
+                            break;
+                        }
+
+                        char lastElement = stack.Pop();
+                        int openingIndex = Array.IndexOf(leftBr, lastElement);
+                        int closingIndex = Array.IndexOf(rightBr, line[i]);
+
+                        if (openingIndex != closingIndex)
+                        {
+                            isBalanced = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/StacksAndQueues-Exercise/08.RecursiveFibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.RecursiveFibonacci
+{
+    class Program
+    {
+        private static long[] numbers;
+
+        public static long GetFibonacci(int n)
+        {
+            if (numbers[n] == 0)
+            {
+                numbers[n] = GetFibonacci(n - 1) + GetFibonacci(n - 2); ;
+            }
+
+            return numbers[n];
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            numbers = new long[n + 2];
+            numbers[1] = 1;
+            numbers[2] = 1;
+            long result = GetFibonacci(n);
+
+            Console.WriteLine(result);         
+        }
+    }
+}
