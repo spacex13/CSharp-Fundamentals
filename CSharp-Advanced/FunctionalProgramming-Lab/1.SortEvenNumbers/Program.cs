e0a39e792b4ee16b9c3dbde8c2ed7386bf2e5705
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-02 16:15:14 +0200

    added exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Lab/1.SortEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Lab/2.SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SumNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
