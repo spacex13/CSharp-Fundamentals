b27c3e2979a1bc459f8dc6cb0df89da539ff23ad
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-04 12:30:01 +0200

    added exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Lab/5.FilterByAge/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.FilterByAge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> people = new Dictionary<string, int>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                var personArgs = Console.ReadLine()
+                    .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                people.Add(personArgs[0], int.Parse(personArgs[1]));
+            }
+
+            string condition = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            Func<int, bool> tester = CreateTester(condition, age);
+            Action<KeyValuePair<string, int>> printer = CreatePrinter(format);
+
+            foreach (var person in people)
+            {
+                if (tester(person.Value))
+                {
+                    printer(person);
+                }
+            }
+        }
+
+        private static Action<KeyValuePair<string, int>> CreatePrinter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return person => Console.WriteLine($"{person.Key}");
+                case "age":
+                    return person => Console.WriteLine($"{person.Value}");
+                case "name age":
+                    return person => Console.WriteLine($"{person.Key} - {person.Value}");
+                default: return null;
+            }
+        }
+
+        private static Func<int, bool> CreateTester(string condition, int age)
+        {
+            switch (condition)
+            {
+                case "younger":
+                    return x => x < age;
+                case "older":
+                    return x => x >= age;
+                default: return null;
+            }
+        }
+    }
+}
