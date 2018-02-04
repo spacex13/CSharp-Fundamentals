3b4e67cdd50e036c55e4e398a2eda38917ec3d08
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-04 12:31:52 +0200

    added exercise

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/09.StackFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.StackFibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<long> stack = new Stack<long>();
+            stack.Push(0);
+            stack.Push(1);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                long firstPopped = stack.Pop();
+                long secondPopped = stack.Peek();
+                long calculated = firstPopped + secondPopped;
+                stack.Push(firstPopped);
+                stack.Push(calculated);
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
