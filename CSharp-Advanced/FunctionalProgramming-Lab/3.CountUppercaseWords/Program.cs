63059d8cd99c50104ff6ae78c81e3e0ffbf0e86d
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-04 12:28:25 +0200

    added exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Lab/3.CountUppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            var line = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => Char.IsUpper(x[0]))
+                .ToList();
+
+            foreach (var word in line)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Lab/4.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AddVAT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:F2}"));
+        }
+    }
+}
