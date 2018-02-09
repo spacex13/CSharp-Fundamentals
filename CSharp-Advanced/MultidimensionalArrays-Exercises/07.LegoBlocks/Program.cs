3b6929ee927265138ff4552bfc3852d7dba65f96
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-09 20:40:40 +0200

    exercises

---
+++ CSharp-Advanced/MultidimensionalArrays-Exercises/07.LegoBlocks/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.LegoBlocks
+{
+    class Program
+    {
+        static int[][] FillJaggedArray(int n)
+        {
+            int[][] jaggedArray = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] rowElements = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jaggedArray[i] = new int[rowElements.Length];
+                
+
+                for (int j = 0; j < rowElements.Length; j++)
+                {
+                    jaggedArray[i][j] = rowElements[j];
+                }
+            }
+
+            return jaggedArray;
+        }
+
+        static bool CheckIfFits(int[][] firstArray, int[][] secondReversed, int n)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                int arrRow1Len = firstArray[i].Length;
+                int arrRow2Len = secondReversed[i].Length;
+                int sum1 = arrRow1Len + arrRow2Len;
+
+                for (int j = 1; j < n; j++)
+                {
+                    arrRow1Len = firstArray[j].Length;
+                    arrRow2Len = secondReversed[j].Length;
+                    int sum2 = arrRow1Len + arrRow2Len;
+
+                    if (sum1 != sum2)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] firstArray = FillJaggedArray(n);
+            int[][] secondArray = FillJaggedArray(n);
+
+            int[][] secondReversed = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] arr = secondArray[i];
+                secondReversed[i] = arr.Reverse().ToArray();
+            }
+
+            bool fits = CheckIfFits(firstArray, secondReversed, n);
+
+            if (fits)
+            {
+                int[][] rectangle = CreateRectangleMatrix(firstArray, secondReversed, n);
+
+                PrintMatrix(n, rectangle);
+            }
+            else
+            {
+                int totalCells = TotalCells(firstArray, secondReversed, n);
+                Console.WriteLine($"The total number of cells is: {totalCells}");
+            }
+        }
+
+        static int TotalCells(int[][] firstArray, int[][] secondReversed, int n)
+        {
+            int cells = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                cells += firstArray[row].Length + secondReversed[row].Length;
+            }
+
+            return cells;
+        }
+
+        static void PrintMatrix(int n, int[][] rectangle)
+        {
+            for (int i = 0; i <n; i++)
+            {
+                Console.WriteLine($"[{string.Join(", ", rectangle[i])}]");
+            }
+        }
+
+        static int[][] CreateRectangleMatrix(int[][] firstArray, int[][] secondReversed, int n)
+        {
+            int[][] rectangle = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] arr1 = firstArray[row];
+                int[] arr2 = secondReversed[row];
+                int totalColumns = arr1.Length + arr2.Length;
+                int count = 0;
+
+                rectangle[row] = new int[totalColumns];
+
+                for (int col = 0; col < totalColumns; col++)
+                {
+                    if (col >= arr1.Length)
+                    {
+                        rectangle[row][col] = arr2[count];
+                        count++;
+                    }
+                    else
+                    {
+                        rectangle[row][col] = arr1[col];
+                    }
+                }
+            }
+
+            return rectangle;
+        }
+    }
+}
---
+++ CSharp-Advanced/MultidimensionalArrays-Exercises/09.Crossfire/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.Crossfire
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int n = dimensions[0];
+            int m = dimensions[1];
+
+            string[][] matrix = FillMatrix(n, m);
+            string command;
+
+            while ((command = Console.ReadLine()) != "Nuke it from orbit")
+            {
+                int[] coordinates = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse).ToArray();
+
+                int nukeRow = coordinates[0];
+                int nukeCol = coordinates[1];
+                int nukeRadius = coordinates[2];
+
+                matrix = NukeCross(nukeRadius, nukeRow, nukeCol, ref matrix);
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(string[][] matrix)
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row.Where(x => x != "")));
+            }
+        }
+
+        static string[][] FillMatrix(int n, int m)
+        {
+            string[][] matrix = new string[n][];
+            int count = 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = new string[m];
+
+                for (int col = 0; col < m; col++)
+                {
+                    matrix[row][col] = count.ToString();
+                    count++;
+                }
+            }
+
+            return matrix;
+        }
+
+        static bool IsInMatrix(int row, int col, string[][] matrix)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+
+        static string[][] NukeCross(int nukeRadius, int nukeRow, int nukeCol, ref string[][] matrix)
+        {
+            for (int rowIndex = nukeRow - nukeRadius; rowIndex <= nukeRow + nukeRadius; rowIndex++)
+            {
+                if (IsInMatrix(rowIndex, nukeCol, matrix))
+                {
+                    matrix[rowIndex][nukeCol] = "";
+                }
+            }
+
+            for (int colIndex = nukeCol - nukeRadius; colIndex <= nukeCol + nukeRadius; colIndex++)
+            {
+                if (IsInMatrix(nukeRow, colIndex, matrix))
+                {
+                    matrix[nukeRow][colIndex] = "";
+                }
+            }
+
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                if (matrix[rowIndex].Any(c => c == ""))
+                {
+                    matrix[rowIndex] = matrix[rowIndex].Where(n => n != "").ToArray();
+                }
+
+                if (matrix[rowIndex].Length < 1)
+                {
+                    matrix = matrix.Take(rowIndex).Concat(matrix.Skip(rowIndex + 1)).ToArray();
+                    rowIndex--;
+                }
+            }
+
+            return matrix;
+        }
+
+        //static string[][] AnotherNukeCrossMethod(int nukeRadius, int nukeRow, int nukeCol, ref string[][] matrix)
+        //{
+        //    if (IsInMatrix(nukeRow, nukeCol, matrix))
+        //    {
+        //        matrix[nukeRow][nukeCol] = "";
+        //    }
+
+        //    for (int j = 1; j <= nukeRadius; j++)
+        //    {
+        //        int leftCol = nukeCol - j;
+        //        int rightCol = nukeCol + j;
+
+        //        int upRow = nukeRow - j;
+        //        int downRow = nukeRow + j;
+
+        //        if (IsInMatrix(nukeRow, leftCol, matrix))
+        //        {
+        //            matrix[nukeRow][leftCol] = "";
+        //        }
+
+        //        if (IsInMatrix(nukeRow, rightCol, matrix))
+        //        {
+        //            matrix[nukeRow][rightCol] = "";
+        //        }
+
+        //        if (IsInMatrix(upRow, nukeCol, matrix))
+        //        {
+        //            matrix[upRow][nukeCol] = "";
+        //        }
+
+        //        if (IsInMatrix(downRow, nukeCol, matrix))
+        //        {
+        //            matrix[downRow][nukeCol] = "";
+        //        }
+        //    }
+
+        //    for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+        //    {
+        //        if (matrix[rowIndex].Any(c => c == ""))
+        //        {
+        //            matrix[rowIndex] = matrix[rowIndex].Where(n => n != "").ToArray();
+        //        }
+
+        //        if (matrix[rowIndex].Length < 1)
+        //        {
+        //            matrix = matrix.Take(rowIndex).Concat(matrix.Skip(rowIndex + 1)).ToArray();
+        //            rowIndex--;
+        //        }
+        //    }
+
+        //    return matrix;
+        //}
+    }
+}
