b6648254e1bd99513cba58e95b9c29069748d19b
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-09 20:41:17 +0200

    exercises

---
+++ CSharp-Advanced/MultidimensionalArrays-Exercises/11.ParkingSystem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _11.ParkingSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int r = dimensions[0];
+            int c = dimensions[1];
+
+            byte[][] parking = new byte[r][];
+            string command;
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                var info = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                      .Select(int.Parse).ToArray();
+                int entryRow = info[0];
+                int x = info[1];
+                int y = info[2];
+
+                if (parking[x] == null)
+                {
+                    parking[x] = new byte[c];
+                }
+
+                if (parking[x][y] == 0)
+                {
+                    parking[x][y] = 1;
+                    int distance = CalcDistance(x, y, entryRow);
+                    Console.WriteLine(distance);
+                }
+                else
+                {
+                    int count = 1;
+                    while (true)
+                    {
+                        int lowerCell = y - count;
+                        int upperCell = y + count;
+
+                        if (lowerCell < 1 && upperCell > c - 1)                     // if cells are out of bounds
+                        {
+                            Console.WriteLine($"Row {x} full");
+                            break;
+                        }
+                        if (lowerCell > 0 && parking[x][lowerCell] == 0)           // if cell is inside of the row and free
+                        {
+                            parking[x][lowerCell] = 1;
+                            int distance = CalcDistance(x, lowerCell, entryRow);
+                            Console.WriteLine(distance);
+                            break;
+                        }
+                        if (upperCell < c && parking[x][upperCell] == 0)            // if cell is inside of the row and free
+                        {
+                            parking[x][upperCell] = 1;
+                            int distance = CalcDistance(x, upperCell, entryRow);
+                            Console.WriteLine(distance);
+                            break;
+                        }
+
+                        count++;
+                    }
+                }
+            }
+        }
+
+        static int CalcDistance(int x, int y, int entryRow)
+        {
+            int distance = 0;
+            distance += Math.Abs(x - entryRow);
+            distance += y + 1;
+
+            return distance;
+        }
+    }
+}
---
+++ CSharp-Advanced/MultidimensionalArrays-Exercises/12.StringMatrixRotation/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _12.StringMatrixRotation
+{
+    class Program
+    {
+        static void Main()
+        {
+            string rotationInfo = Console.ReadLine();
+            string pattern = @"[\d]+";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(rotationInfo);
+            int degrees = int.Parse(match.ToString());
+
+            string line;
+            List<string> lines = new List<string>();
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                lines.Add(line);
+            }
+
+            int columns = 0;
+            int rows = lines.Count;
+
+            foreach (var word in lines)
+            {
+                if (word.Length > columns)
+                    columns = word.Length;
+            }
+
+            char[,] matrix = new char[rows, columns];
+            int count = 0;
+
+            if (degrees > 360)
+            {
+                degrees %= 360;
+            }
+
+            switch (degrees)
+            {
+                case 0:
+                    count = 0;
+                    break;
+                case 90:
+                    count = 1;
+                    break;
+                case 180:
+                    count = 2;
+                    break;
+                case 270:
+                    count = 3;
+                    break;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                string currWord = lines[i];
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j >= currWord.Length)
+                    {
+                        matrix[i, j] = ' ';
+                    }
+                    else
+                    {
+                        matrix[i, j] = currWord[j];
+                    }
+                }
+            }
+            
+            for (int i = 0; i < count; i++)
+            {
+                matrix = ReverseMatrix(matrix);
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] ReverseMatrix(char[,] matrix)
+        {
+            int oldRows = matrix.GetLength(0);
+            int oldCols = matrix.GetLength(1);
+            char[,] newMatrix = new char[oldCols, oldRows];
+
+            int newRows = 0;
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int newCols = 0;
+                for (int j = matrix.GetLength(0) - 1; j >= 0; j--)
+                {
+                    newMatrix[newRows, newCols] = matrix[j, i];
+                    newCols++;
+                }
+
+                newRows++;
+            }
+
+            return newMatrix;
+        }
+    }
+}
