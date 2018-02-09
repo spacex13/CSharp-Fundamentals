523ded648484247e2a3ddc1472c3e9262f4a0bc4
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-09 20:42:34 +0200

    exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/01.ActionPrint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string> print = x => Console.WriteLine(x);
+
+            var input = Console.ReadLine().Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var name in input)
+            {
+                print(name);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string> print = x => Console.WriteLine("Sir " + x);
+
+            var input = Console.ReadLine().Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var name in input)
+            {
+                print(name);
+            }
+        }
+    }
+}
