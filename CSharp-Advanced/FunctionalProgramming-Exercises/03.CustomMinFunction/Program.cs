716363dac590275658ee3983c9571f6025ebc498
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-09 20:43:21 +0200

    exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/03.CustomMinFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, int, int> findMin = (x, y) =>
+            {
+                if (x > y)
+                {
+                    return y;
+                }
+                return x;
+            };
+
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int minValue = Int32.MaxValue;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentMin = findMin(numbers[0], numbers[i]);
+
+                minValue = findMin(minValue, currentMin);
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int start = numbers[0];
+            int end = numbers[1];
+
+            List<int> nums = new List<int>();
+
+            string str = Console.ReadLine();
+
+            Func<int, bool> odds = x => x % 2 != 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                if (str == "odd" && odds(i))
+                {
+                    nums.Add(i);
+                }
+                else if (str == "even" && odds(i) == false)
+                {
+                    nums.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
