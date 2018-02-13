27b3691823027aec2015f22a03d3f87e6b0dea19
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-13 18:52:47 +0200

    exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/09.ListOfPredicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, bool> func = (x, y) => x % y == 0;
+            List<int> sequence = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool divisible = true;
+
+                foreach (var num in numbers)
+                {
+                    if (func(i, num) == false)
+                    {
+                        divisible = false;
+                        break;
+                    }
+                }
+
+                if (divisible)
+                    sequence.Add(i);
+            }
+
+            Console.WriteLine(string.Join(" ", sequence));
+        }
+    }
+}
