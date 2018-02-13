7bd9078385f667f785d14dd3d9d52415a24cbb2c
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-13 18:52:17 +0200

    exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/07.PredicateForNames/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> fitsLength = (name, len) =>
+            {
+                if (name.Length <= len)
+                    return true;
+                return false;
+            };
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            foreach (var name in names)
+            {
+                if (fitsLength(name, length))
+                {
+                    print(name);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/08.CustomComparator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> comparator = (x, y) =>
+            (x % 2 == 0 && y % 2 != 0) ? -1 :
+            (x % 2 != 0 && y % 2 == 0) ? 1 :
+             x.CompareTo(y);
+
+            Array.Sort<int>(numbers, new Comparison<int>(comparator));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
