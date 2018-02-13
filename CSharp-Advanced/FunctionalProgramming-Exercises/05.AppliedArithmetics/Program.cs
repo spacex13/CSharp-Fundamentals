acb5b4d085f8398da0b8f1461be6b5eeb8de4e1e
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-02-13 18:51:54 +0200

    exercises

---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = "";
+
+            Func<int, int> addition = x => x += 1;
+            Func<int, int> subtract = x => x -= 1;
+            Func<int, int> multiply = x => x *= 2;
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = numbers.Select(addition).ToArray();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(subtract).ToArray();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(multiply).ToArray();
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/FunctionalProgramming-Exercises/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int, int, bool> divisibleByN = (x, N) => x % N == 0;
+
+            numbers.Reverse();
+
+            for (int num = 0; num < numbers.Count; num++)
+            {
+                if (divisibleByN(numbers[num], n))
+                {
+                    numbers.Remove(numbers[num]);
+                    num--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
