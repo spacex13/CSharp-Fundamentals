33b75db9480a87df053c33ea9543dc3d7b3e2a99
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-04-05 17:14:49 +0300

    Iterators And Comparators - Lab

---
+++ CSharp-OOP-Advanced/IteratorsAndComparators-Lab/2.LibraryIterator/Library.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        this.Books = new List<Book>(books);
+    }
+
+    public List<Book> Books { get; set; }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.Books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        private IReadOnlyList<Book> books;
+        private int currentIndex;
+
+        public Book Current => this.books[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose() {}
+
+        public bool MoveNext()
+        {
+            this.currentIndex++;
+
+            return this.currentIndex < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1;
+        }
+    }
+}
---
+++ CSharp-OOP-Advanced/IteratorsAndComparators-Lab/3.ComparableBook/Library.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Library : IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        this.books = new SortedSet<Book>(books);
+    }
+
+    public SortedSet<Book> books;
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>(books);
+        }
+
+        private IList<Book> books;
+        private int currentIndex;
+
+        public Book Current => this.books.ToList()[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose() {}
+
+        public bool MoveNext()
+        {
+            this.currentIndex++;
+
+            return this.currentIndex < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1;
+        }
+    }
+}
